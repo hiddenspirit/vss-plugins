d674e7d9f3b61a54760da526e9a7ac8a702bc162
nathbot <nathbot@gmail.com@6362f998-438c-56ef-bf46-1fa3753ed092>
2011-07-20 08:37:27 +0000

    R?organisation des dossiers

--- public/make_packs.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-del "packs\*.zip"
-"c:\program files\7-zip\7z.exe" a -tzip -mx9 -r "packs\SW_US" "jsplugin\*.js" "jsplugin\*.txt" "presets\*_US_*.ini" "dict\_about_dictionaries_.txt" "dict\perso.dic" "dict\fr*.aff" "dict\fr*.dic" "dict\*fr*.txt" "dict\en_US*.aff" "dict\en_US*.dic" "dict\*en_US*.txt" "readme*.txt"
-"c:\program files\7-zip\7z.exe" a -tzip -mx9 -r "packs\SW_UK" "jsplugin\*.js" "jsplugin\*.txt" "presets\*_UK_*.ini" "dict\_about_dictionaries_.txt" "dict\perso.dic" "dict\fr*.aff" "dict\fr*.dic" "dict\*fr*.txt" "dict\en_GB*.aff" "dict\en_GB*.dic" "dict\*en_GB*.txt" "readme*.txt"
