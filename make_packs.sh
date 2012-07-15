d4282f678541b78add3ede6984f211699c3e13f5
spirit <hiddenspirit@gmail.com>
2012-07-15 04:09:38 +0200

    Remove packs from repository.

--- make_packs.sh
+++ make_packs.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [ ! -d packs ]
+then
+  mkdir packs
+fi
 rm packs/*.zip
 zip -9 -R packs/SW_US jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_US_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_US* readme*.txt
 zip -9 -R packs/SW_UK jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_UK_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_GB* readme*.txt
