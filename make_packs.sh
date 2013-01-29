6cfc4d6aa38b64df7c12de741e9a6151fba636e7
spirit <hiddenspirit@gmail.com>
2013-01-29 08:53:26 +0100

    More "international" naming for presets

--- make_packs.sh
+++ make_packs.sh
@@ -4,5 +4,5 @@ then
   mkdir packs
 fi
 rm packs/*.zip
-zip -9 -R packs/SW_US jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_US_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_US* readme*.txt
-zip -9 -R packs/SW_UK jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_UK_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_GB* readme*.txt
+zip -9 -R packs/SW_NTSC jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_NTSC_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_US* readme*.txt
+zip -9 -R packs/SW_PAL jsplugin/*.js jsplugin/*.txt jsplugin/*/*.js jsplugin/*/*.txt presets/*_PAL_*.ini dict/_about_dictionaries_.txt dict/perso.dic dict/*fr* dict/*en_GB* readme*.txt
